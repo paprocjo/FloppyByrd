801eeab8d674a7f2049d10175811ee3fce0bb4d4
Jonathan Paprocki <paprocjo@gmail.com>
2015-12-09 22:22:39 -0600

    Initial Commit

---
+++ FloppyByrd/Assets/Scripts/Background.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform mountains;
+
+    private float groundSpeed = 10;
+    private float mountainSpeed = 5;
+
+    #region Unity
+	void Update () 
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * groundSpeed);
+        mountains.Rotate(Vector3.down * Time.deltaTime * mountainSpeed);
+    }
+    #endregion
+}
